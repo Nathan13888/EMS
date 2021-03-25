27b65cf6f4921e84b9e1639523b3f5b7653c992c
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-03-25 09:25:37 -0400

    add Address
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

---
+++ src/net/pdsb/nathan13888/ems/types/Address.java
@@ -0,0 +1,18 @@
+package net.pdsb.nathan13888.ems.types;
+
+public class Address {
+
+	private String address;
+
+	public Address(String address) {
+		this.address = address;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
