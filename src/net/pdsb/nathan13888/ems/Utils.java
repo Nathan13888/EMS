71461517414a57a76839ba0f1ce0ecdddf82edf3
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-04-15 12:31:15 -0400

    add feature to open webpage
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- src/net/pdsb/nathan13888/ems/Utils.java
+++ src/net/pdsb/nathan13888/ems/Utils.java
@@ -1,5 +1,9 @@
 package net.pdsb.nathan13888.ems;
 
+import java.awt.Desktop;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class Utils {
 
 	public static String toTitleCase(String s) {
@@ -8,5 +12,29 @@ public class Utils {
 		} else {
 			return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
 		}
+	}
+
+	public static boolean openWebpage(String s) {
+		try {
+			return openWebpage(new URI(s));
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public static boolean openWebpage(URI uri) {
+		System.out.println("Opening webpage " + uri.toString());
+		Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+		if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+			try {
+				desktop.browse(uri);
+				return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
 	}
 }
