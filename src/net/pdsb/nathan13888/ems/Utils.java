0e61b85a41607f972ad0c2bb70b7f56051d0555e
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-04-18 11:58:54 -0400

    add file info features
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- src/net/pdsb/nathan13888/ems/Utils.java
+++ src/net/pdsb/nathan13888/ems/Utils.java
@@ -1,10 +1,17 @@
 package net.pdsb.nathan13888.ems;
 
 import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Utils {
 
@@ -14,6 +21,43 @@ public class Utils {
 		} else {
 			return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
 		}
+	}
+
+	public static String getFileCreation(File file) {
+		BasicFileAttributes attrs;
+		try {
+			attrs = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+			FileTime time = attrs.creationTime();
+
+			String pattern = "yyyy-MM-dd HH:mm:ss";
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+			String formatted = simpleDateFormat.format(new Date(time.toMillis()));
+
+//			System.out.println("The file creation date and time is: " + formatted);
+			return formatted;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "ERROR...";
+	}
+
+	public static String getFileModified(File file) {
+		BasicFileAttributes attrs;
+		try {
+			attrs = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+			FileTime time = attrs.lastModifiedTime();
+
+			String pattern = "yyyy-MM-dd HH:mm:ss";
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+			String formatted = simpleDateFormat.format(new Date(time.toMillis()));
+
+			return formatted;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "ERROR...";
 	}
 
 	public static boolean openWebpage(String s) {
